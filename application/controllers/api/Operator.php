fd2bfe7df5c0af44c52565486f115ecbdaeb4f15
Asep Maryana <asep.maryana@gmail.com>
2017-07-20 17:21:05 +0700

    add crud operator

---
+++ application/controllers/api/Operator.php
@@ -0,0 +1,55 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+// This can be removed if you use __autoload() in config.php OR use Modular Extensions
+require APPPATH . '/libraries/REST_Controller.php';
+
+class Operator extends REST_Controller 
+{	
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->model('OperatorModel','',TRUE);
+	}
+    
+    function fetch_get()
+    {
+        $page    = $this->uri->segment(4);
+        $size    = $this->uri->segment(5);
+        if(empty($page) || $page == '0') $page = 1;
+        if(empty($size) || $size == '0') $size = 10;
+        $offset  = ($page-1)*$size;
+        
+        $rows   = $this->OperatorModel->get_paged_list($size, $offset)->result();
+        $total  = $this->OperatorModel->count_all();
+        $totalPage  = ceil($total/$size);
+        $firstPage  = ($page == 0 || $page == 1) ? true : false;
+        $lastPage   = ($page == $totalPage) ? true : false;
+        $msg        = array('content'=>$rows, 'totalPage'=>$totalPage, 'firstPage'=>$firstPage, 'lastPage'=>$lastPage, 'page'=>intval($page), 'total'=>$total);
+        $this->response($msg, REST_Controller::HTTP_OK);
+    }
+    
+    function save_post()
+    {
+        $values = json_decode(file_get_contents('php://input'), true);        
+        $id     = $this->OperatorModel->save($values);
+        $values['id']   = $id;
+        $this->response($values, REST_Controller::HTTP_CREATED);
+    }
+    
+    function update_post()
+    {
+        $id    = $this->uri->segment(4);
+        $values = json_decode(file_get_contents('php://input'), true);
+        $this->OperatorModel->update($id, $values);
+        $this->response($values, REST_Controller::HTTP_OK);
+    }
+    
+    function remove_delete()
+    {
+        $id    = $this->uri->segment(4);
+        $this->OperatorModel->delete($id);
+        $this->response(array('succeed'=>true), REST_Controller::HTTP_OK);
+    }
+}
+?>
---
+++ application/models/OperatorModel.php
@@ -0,0 +1,46 @@
+<?php
+class OperatorModel extends CI_Model 
+{
+	public $table	= 'operator';
+
+	function __construct()
+	{
+		parent::__construct();
+	}
+	
+	function count_all()
+	{
+		return $this->db->count_all($this->table);
+	}
+	
+	function get_paged_list($limit = 10, $offset = 0)
+	{
+		$this->db->order_by('name','asc');
+		return $this->db->get($this->table, $limit, $offset);
+	}
+
+	function get_by_id($id)
+	{
+		$this->db->where('id', $id);
+		return $this->db->get($this->table);
+	}
+	
+	function save($data)
+	{
+		$this->db->insert($this->table, $data);
+		return $this->db->insert_id();
+	}
+	
+	function update($id, $data)
+	{
+		$this->db->where('id', $id);
+		$this->db->update($this->table, $data);
+	}
+	
+	function delete($id)
+	{
+		$this->db->where('id', $id);
+		$this->db->delete($this->table);
+	}
+}
+?>
